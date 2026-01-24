89730e563323e5cf4e8013f589df308abb802a90
Andreas Lüdtke <andi.luedtke@gmx.de>
2026-01-24 17:07:46 +0100

    Dependency updates, updated Flatpak build dependencies

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,6 @@ import Foundation
 import audioplayers_darwin
 import hid_listener
 import package_info_plus
-import path_provider_foundation
 import screen_retriever_macos
 import shared_preferences_foundation
 import url_launcher_macos
@@ -18,7 +17,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   HidListenerPlugin.register(with: registry.registrar(forPlugin: "HidListenerPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
